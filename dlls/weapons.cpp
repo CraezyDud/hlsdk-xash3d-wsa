33f9d2ce70dbb9ce911ec5873ae46cd23968dc24
MrSwedish <84625713+MrSwedish@users.noreply.github.com>
2021-06-01 19:35:57 +0200

    Update weapons.cpp

--- dlls/weapons.cpp
+++ dlls/weapons.cpp
@@ -979,11 +979,28 @@ BOOL CBasePlayerWeapon::DefaultDeploy( const char *szViewModel, const char *szWe
 
 BOOL CBasePlayerWeapon::DefaultReload( int iClipSize, int iAnim, float fDelay, int body, bool LeaveInChamber )
 {
-	LeaveInChamberGL = LeaveInChamber;
+	#ifdef LEAVE_AMMO_IN_CLIP
+		LeaveInChamberGL = LeaveInChamber;
+	#else
+		LeaveInChamber = FALSE;
+	#endif
 	if( m_pPlayer->m_rgAmmo[m_iPrimaryAmmoType] <= 0 )
 		return FALSE;
-
-	int j = Q_min( iClipSize - m_iClip, m_pPlayer->m_rgAmmo[m_iPrimaryAmmoType] );
+	
+	#ifdef LEAVE_AMMO_IN_CLIP
+		int j = 0;
+			
+		if( (LeaveInChamber == TRUE) && (m_iClip<1) )
+		{
+			j = Q_min( iClipSize - m_iClip-1, m_pPlayer->m_rgAmmo[m_iPrimaryAmmoType] );
+		}
+		else
+		{
+			j = Q_min( iClipSize - m_iClip, m_pPlayer->m_rgAmmo[m_iPrimaryAmmoType] );
+		}
+	#else
+		int j = Q_min( iClipSize - m_iClip, m_pPlayer->m_rgAmmo[m_iPrimaryAmmoType] );
+	#endif
 
 	if( j == 0 )
 		return FALSE;
